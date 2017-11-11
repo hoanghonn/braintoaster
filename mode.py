d3dd0588f0e105dd3dd3bc4704798c1bd608687c
hoanghonn <hoangnguyenthai97@gmail.com>
2017-11-11 13:58:24 -0500

    Add basic functions to Manager

---
+++ mode.py
@@ -0,0 +1,22 @@
+
+
+class Mode():
+
+    def __init__(self, screen):
+        return
+
+    def play_game(self):
+        return None
+
+
+class MathGame(Mode):
+    # implement init later
+    def play_game(self):
+
+        return True
+
+
+class ColorGame(Mode):
+    # implement init later
+    def play_game(self):
+        return True
