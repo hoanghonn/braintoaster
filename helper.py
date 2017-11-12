e0a2670bc8abcaec115dc823f1d3e18beeb2b66b
hoanghonn <hoangnguyenthai97@gmail.com>
2017-11-11 19:37:40 -0500

    Reduce rang from 1 to 10

--- helper.py
+++ helper.py
@@ -1,15 +1,17 @@
 import pygame, sys
 from pygame import *
 
+
 def font_blit(screen, center, size, text, color, background_color = None):
     FONT = pygame.font.Font('FreeSansBold.ttf', size)
-    TEXT = 'none';
+    TEXT = 'none'
     if background_color == None:
         TEXT = FONT.render(text, True, color)
     else:
         TEXT = FONT.render(text, True, color, background_color)
     screen.blit(TEXT, center)
-    return (TEXT.get_rect().width, TEXT.get_rect().height)
+    return TEXT.get_rect().width, TEXT.get_rect().height
+
 
 def terminate():
     pygame.quit()
